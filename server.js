4e878ef024ba362bc9a3c05b8b9357c019c3936e
dvicjon <devinvjones@gmail.com>
2021-04-17 22:04:44 -0400

    rearranged files, everything working:

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+  
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -22,7 +23,7 @@ mongoose.connect(MONGODB_URI, {
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require('./routes/api'));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
